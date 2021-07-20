f714adb2ad2cf55950dba2139236f3483370e187
Vynex <kushagra100910@gmail.com>
2021-07-20 17:59:18 +0530

    Added YouTube Search Functionality

--- client/logifront/src/TrackSearchResult.js
+++ client/logifront/src/TrackSearchResult.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
 // const YoutubeMusicApi = require("youtube-music-api");
 
 export default function TrackSearchResult({ track, chooseTrack }) {
-  function handlePlay() {
-    // const api = new YoutubeMusicApi();
-    // api.initalize().then((info) => {
-    //   api.search(track.title + " " + track.artist).then((results) => {
-    //     streamId(results);
-    //     console.log(results);
-    //   });
-    // });
+	async function handlePlay() {
+		// const api = new YoutubeMusicApi();
+		// api.initalize().then((info) => {
+		//   api.search(track.title + " " + track.artist).then((results) => {
+		//     streamId(results);
+		//     console.log(results);
+		//   });
+		// });
 
-    chooseTrack(track);
-  }
-  return (
-    <div
-      className="d-flex m-2 align-items-center"
-      style={{ cursor: "pointer" }}
-      onClick={handlePlay}
-    >
-      <img src={track.albumUrl} style={{ height: "64px", width: "64px  " }} />
-      <div className="ml-3">
-        <div className="px-4"> {track.title} </div>
-        <div className="text-muted px-4"> {track.artist} </div>
-      </div>
-    </div>
-  );
+		chooseTrack(track);
+		const song = { title: track.title, artist: track.artist };
+
+		await axios.post('http://localhost:3001/song', { song });
+	}
+	return (
+		<div
+			className="d-flex m-2 align-items-center"
+			style={{ cursor: 'pointer' }}
+			onClick={handlePlay}
+		>
+			<img
+				src={track.albumUrl}
+				style={{ height: '64px', width: '64px  ' }}
+			/>
+			<div className="ml-3">
+				<div className="px-4"> {track.title} </div>
+				<div className="text-muted px-4"> {track.artist} </div>
+			</div>
+		</div>
+	);
 }
--- server/server.js
+++ server/server.js
@@ -1,73 +1,96 @@
-const express = require("express");
+const express = require('express');
 
-const cors = require("cors");
+const cors = require('cors');
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-const SpotifyWebApi = require("spotify-web-api-node");
+const lyricsFinder = require('lyrics-finder');
 
-const lyricsFinder = require("lyrics-finder");
+const ytdl = require('ytdl-core');
+const YoutubeMusicApi = require('youtube-music-api');
+const SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyToYoutube = require('spotify-to-youtube');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "e6b0eaeb0346417db36520eb93e86612",
-    clientSecret: "42b2a21559a34015a0d62fa2e54335a5",
-    refreshToken: refreshToken,
-  });
-
-  // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
-  spotifyApi
-    .refreshAccessToken()
-    .then((data) => {
-      res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
-      });
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body["access_token"]);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(400);
-    });
+const spotifyApi = new SpotifyWebApi({
+	redirectUri: 'http://localhost:3000',
+	clientId: 'e6b0eaeb0346417db36520eb93e86612',
+	clientSecret: '42b2a21559a34015a0d62fa2e54335a5',
 });
 
-app.post("/login", (req, res) => {
-  const code = req.body.code;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "e6b0eaeb0346417db36520eb93e86612",
-    clientSecret: "42b2a21559a34015a0d62fa2e54335a5",
-  });
+app.post('/refresh', (req, res) => {
+	const refreshToken = req.body.refreshToken;
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      });
-    })
-    .catch((err) => {
-      res.sendStatus(400);
-      console.log(err);
-    });
+	// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+	spotifyApi
+		.refreshAccessToken()
+		.then((data) => {
+			res.json({
+				accessToken: data.body.accessToken,
+				expiresIn: data.body.expiresIn,
+			});
+			// Save the access token so that it's used in future calls
+			spotifyApi.setAccessToken(data.body['access_token']);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(400);
+		});
 });
 
-app.get("/lyrics", async (req, res) => {
-  const lyrics =
-    (await lyricsFinder(req.query.artist, req.query.track)) ||
-    "No Lyrics Found! :( ";
-  res.json({ lyrics });
+app.post('/login', (req, res) => {
+	const code = req.body.code;
+
+	spotifyApi
+		.authorizationCodeGrant(code)
+		.then((data) => {
+			res.json({
+				accessToken: data.body.access_token,
+				refreshToken: data.body.refresh_token,
+				expiresIn: data.body.expires_in,
+			});
+		})
+		.catch((err) => {
+			res.sendStatus(400);
+			console.log(err);
+		});
+});
+
+app.get('/lyrics', async (req, res) => {
+	const lyrics =
+		(await lyricsFinder(req.query.artist, req.query.track)) ||
+		'No Lyrics Found! :( ';
+	res.json({ lyrics });
+});
+
+app.post('/song', async (req, res) => {
+	const { song } = req.body;
+
+	console.log(song);
+	const api = new YoutubeMusicApi();
+
+	async function getStreamUrl(songId) {
+		let info = await ytdl.getInfo(songId);
+		let format = ytdl.chooseFormat(info.formats, { quality: '140' });
+
+		return format.url;
+	}
+
+	api.initalize().then((info) => {
+		api.search(`${song.title} ${song.artist}`).then((result) => {
+			console.log(result.content[0].videoId);
+			const videoId = result.content[0].videoId;
+
+			console.log(getStreamUrl(videoId));
+		});
+	});
+
+	res.status(200);
 });
 
 // app.get("/song", async (req, res) => {
